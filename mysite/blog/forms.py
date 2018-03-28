d696268e81d6bf421e8ef868622db62311bdcf7a
ulimit65535 <2510823550@qq.com>
2018-03-28 14:18:43 +0800

    搞不定

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -16,3 +16,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,6 +3,7 @@
 
 from django.conf.urls import url
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name='blog'
 
@@ -14,5 +15,7 @@ urlpatterns = [
         views.post_detail,
         name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share,
-        name='post_share')
+        name='post_share'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
+    url(r'^search/$', views.post_search, name='post_search'),
 ]
